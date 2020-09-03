af40f7b28efd344b3496cbb11ddccf1d350fbbb1
ducky123456 <54424682+ducky123456@users.noreply.github.com>
2020-09-02 21:24:07 -0400

    Initial commit

---
+++ src/main/java/com/crayon_duck/adventurecraft/AdventureCraft.java
@@ -0,0 +1,41 @@
+package com.crayon_duck.adventurecraft;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("adventurecraft")
+public class AdventureCraft
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public AdventureCraft() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    	
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) 
+    {
+    	
+    }
+    
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) 
+    {
+    	
+    }
+}
